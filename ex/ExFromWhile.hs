a9b877c80f8f8c3f8468423586019172b736a7ae
igorvicentels <igorvicente97@gmail.com>
2022-09-21 12:05:36 -0300

    add exFromWhile

---
+++ ex/ExFromWhile.hs
@@ -0,0 +1,14 @@
+module ExFromWhile where
+
+fromWhile :: Int -> (a -> Bool) -> [a] -> [a]
+fromWhile n p = takeWhile p . drop n
+
+fromFor :: Int -> Int -> [a] -> [a]
+fromFor n m = take m . drop n
+
+fromTo :: Int -> Int -> [a] -> [a]
+-- fromTo n m = take (m - n + 1) . drop n
+fromTo n m = fromFor n (m - n + 1)
+
+fromToThat :: Int -> Int -> (a -> Bool) -> [a] -> [a]
+fromToThat n m p = filter p . fromTo n m
