d7bf9c89ea455e3bb3b10b111aaa286d19bd84c6
igorvicentels <igorvicente97@gmail.com>
2022-10-26 22:14:26 -0300

    add ExEither.hs

---
+++ ex/ExEither.hs
@@ -0,0 +1,46 @@
+module ExEither where
+
+-- Do not alter this import!
+import Prelude hiding ( either, Either(..) )
+import qualified Data.Either as E
+
+data Either a b = Left a | Right b
+    deriving (Show, Eq)
+
+isLeft :: Either a b -> Bool
+isLeft (Left _) = True
+isLeft _         = False
+
+isRight :: Either a b -> Bool
+isRight (Right _) = True
+isRight _         = False
+
+lefts :: [Either a b] -> [a]
+lefts [] = [] 
+lefts (Left x : xs) = x : lefts xs 
+lefts (_      : xs) = lefts xs 
+
+rights :: [Either a b] -> [b]
+rights [] = []
+rights (Right x : xs) = x : rights xs
+rights (_       : xs) = rights xs
+
+fromLeft :: a -> Either a b -> a
+fromLeft _ (Left x)  = x
+fromLeft d (Right _) = d
+
+fromRight :: b -> Either a b -> b
+fromRight d (Left _) = d
+fromRight _ (Right x) = x
+
+partitionEithers :: [Either a b] -> ([a], [b])
+partitionEithers []            = ([], [])
+partitionEithers (Left x : xs) = (x : ls, rs)
+    where (ls,rs) = partitionEithers xs
+partitionEithers (Right x : xs) = (ls, x : rs)
+    where (ls,rs) = partitionEithers xs
+
+either :: (a -> c) -> (b -> c) -> Either a b -> c
+either f g (Left x)  = f x
+either f g (Right x) = g x
+
